034f1d097539f2ecc0a0e5a5500df4acbeb5bb99
Godzilogg <photon@Matthew-Vincek-Mac.home>
2019-03-18 22:08:38 -0400

    removing unused java import

--- src/Migi.java
+++ src/Migi.java
@@ -8,7 +8,6 @@ import java.nio.ByteOrder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 
